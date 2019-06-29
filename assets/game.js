6bde815fd74521431f6b96bc7f348c0b427c3db0
lidia <lidiabara7@gmail.com>
2019-06-28 20:31:28 -0700

    changed the order of the links in the html file so that the link for firebase was before the js. Also fixed a few typos in the js file

--- assets/game.js
+++ assets/game.js
@@ -1,11 +1,10 @@
-
   // Your web app's Firebase configuration
   var firebaseConfig = {
     apiKey: "AIzaSyBjWGazSYipVG0KWuNE3FiZ5_NsGfJEB1E",
     authDomain: "train-update.firebaseapp.com",
     databaseURL: "https://train-update.firebaseio.com",
     projectId: "train-update",
-    storageBucket: "",
+    storageBucket: "train-update.appspot.com",
     messagingSenderId: "223401844616",
     appId: "1:223401844616:web:504dd58d0fbae2d6"
   };
@@ -18,13 +17,12 @@ var database = firebase.database();
 //need to create a button for users to add their train info and update the html file.
 $("#add-train").on("click", function (event) {
   event.preventDefault();
-})
 
 //garab info from the user input
-var trainName = ("#Trainname-input").val().trim();
-var destination = ("#destination-input").val().trim();
-var firstTrain = ("#First-train-time").val().trim();
-var frequency = ("#frequency-input").val().trim();
+var trainName = $("#Trainname-input").val().trim();
+var destination = $("#destination-input").val().trim();
+var firstTrain = $("#First-train-time").val().trim();
+var frequency = $("#frequency-input").val().trim();
 
 //create a local temp object to hold train data
 var trainData = {
@@ -33,22 +31,26 @@ var trainData = {
   firstTrain: firstTrain,
   frequency: frequency,
 };
-
 //to upload train data to the database
 database.ref().push(trainData);
-console.log(trainName);
-console.log(destination);
-console.log(firstTrain);
-console.log(frequency);
+
+console.log(trainData.name);
+console.log(trainData.destination);
+console.log(trainData.firstTrain);
+console.log(trainData.frequency);
 
 //to clear the text boxes
 $("#Trainname-input").val("");
 $("#destination-input").val("");
 $("#First-train-time").val("");
 $("#frequency-input").val("");
+});
 
 //to get the user input from firebase to the html file
-database.ref().on("child_added", function (childSnapshot) {
+database.ref().on("child_added", function (childSnapshot){
+  console.log(childSnapshot.val());
+
+  //storing info into a variable
   var trainName = childSnapshot.val().name;
   var traindestination = childSnapshot.val().destination;
   var firstTrain = childSnapshot.val().firstTrain;
@@ -62,9 +64,9 @@ database.ref().on("child_added", function (childSnapshot) {
     $("<td>").text(traindestination),
     $("<td>").text(firstTrain),
     $("<td>").text(frequency),
-  )
+  );
 
-  $("#tableBody").append(newRow);
+  $("#tableBody > tbody").append(newRow);
 })
 
     //calculate when the next train will arrive, relative to the current time.
